cc8397e28ef06fe3b8a651687f393ea3d99bf208
Mikaias Santos <mikaiassantos28@gmail.com>
2024-06-18 21:12:59 -0300

    Adicionando novos arquivos para o deploy no Netlify

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
+// Para começar a medir o desempenho do seu aplicativo, passe uma função
+// para registrar resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint analítico. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
